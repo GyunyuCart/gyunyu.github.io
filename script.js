aed64e6627cd4d7a2d992dff49442c1d6453ed41
GyunyuCart <132253235+GyunyuCart@users.noreply.github.com>
2023-05-01 18:13:23 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-<script>
-  window.addEventListener('load', function() {
-    var loader = document.querySelector('.loader-container');
-    loader.classList.add('hide');
-    var content = document.querySelector('.wrapper');
-    content.style.display = 'block';
-  });
-</script>
