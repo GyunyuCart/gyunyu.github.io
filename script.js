20af2edde8d121820f527dbfe5b28e970fc04012
GyunyuCart <132253235+GyunyuCart@users.noreply.github.com>
2023-05-01 03:10:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  window.addEventListener('load', function() {
+    var loader = document.querySelector('.loader-container');
+    loader.classList.add('hide');
+    var content = document.querySelector('.wrapper');
+    content.style.display = 'block';
+  });
+</script>
